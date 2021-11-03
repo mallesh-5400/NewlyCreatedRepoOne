8ab37ef0b6f2cb8ad15ccc7ed25b54daf20d02ee
mallesh-5400 <62804698+mallesh-5400@users.noreply.github.com>
2021-11-03 16:58:19 +0530

    Implemented addNewFeatures method

--- SwiftFile.swift
+++ SwiftFile.swift
@@ -1,16 +1,19 @@
 protocol AddingNewFeatures {
+  var cookieSize: Int{ get set}
   func addNewFeature()
   func removeNewFeature()
 Struct Cookies {
   int numberOfCookies: Int
   isItTastie: Bool
 }
-class CookieTaste {
+class CookieTaste: AddingNewFeatures {
   var cookie: Cookies?
   func cookiesQuantity() {
     cookie.numberOfCookies = 10
     cookie.isItTastie = true
   }
+  func addNewFeatures() {
+    cookieSize += cookie.numberOfCookies
 }
 
 let cookieInstance = CookieTaste()
