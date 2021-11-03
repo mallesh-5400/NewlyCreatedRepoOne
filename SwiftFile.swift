3f8d409dca75b16533468d8065b44de8a709a0f3
mallesh-5400 <62804698+mallesh-5400@users.noreply.github.com>
2021-11-03 23:06:05 +0530

    This file only contains structure

--- SwiftFile.swift
+++ SwiftFile.swift
@@ -1,28 +1,6 @@
-protocol AddingNewFeatures {
-  var cookieSize: Int{ get set}
-  func addNewFeature()
-  func removeNewFeature()
-}
-
 Struct Cookies {
   int numberOfCookies: Int
   isItTastie: Bool
 }
-  
-class CookieTaste: AddingNewFeatures {
-  
-  var cookie: Cookies?
-   func cookiesQuantity() {
-    
-    cookie.numberOfCookies = 10
-    cookie.isItTastie = true
-    
-  }
-  
-  func addNewFeatures() {
-    cookieSize += cookie.numberOfCookies
-}
-
-
 End of the protocol branch 
  
